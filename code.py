1023158ee8cdcfd92f5eaee37052e8c2439eb9ea
Allenki Akshay <111362177+allenkiakshay@users.noreply.github.com>
2023-06-13 13:47:57 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -13,12 +13,18 @@ def xml_to_csv(file_path, file_name) -> None:
         num_records = len(root)
 
         for i in range(num_records):
-            rec = (child.text for child in root[i])
+            speaker = (root[i].attrib.get('speaker'))
+            rec = [f"[ {speaker} ] : "] + [child.text for child in root[i]]
             writer.writerow(rec)
 
 
+    # with open(s_name, 'r') as in_f, open(s_name, 'w') as out_f:
+    #     content = in_f.read().replace(',',' ')
+    #     out_f.write(content)
 
-file_path = './xml trans/4oLp3bc9OSJbDrwM 1.xml'   #Enter Your Specified path where xml file is stored
+
+
+file_path = './xml trans/ZQC2TFqLsvNDq9bP.xml'
 file_name = os.path.splitext(os.path.basename(file_path))[0]
 file1_name = ("./CSV Files/" + str(file_name) + ".csv")
 
